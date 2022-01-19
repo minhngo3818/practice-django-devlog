1b4edd9cf15b24bea50ee1a26e6a742fa349b47a
minh minh <eric@example.com>
2022-01-18 21:59:56 -0800

    update render user register and login

--- users/views.py
+++ users/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 def loginUser(request):
-
+    page = 'login'
     # Always redirect to profile page from login whenever user is authenticated
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -36,8 +38,37 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User logged out!')
+    messages.info(request, 'User logged out!')
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            # Explain commit=False: it will create a temporary
+            # instance of just-registered user object
+            # so that frontend and backend can process
+            # the data just in case it causes an issue
+            # eg: some identical username but different format lower case vs. upper case
+
+            user.username = user.username.lower()   # convert to lower case make username all unique
+            user.save()
+
+            messages.success(request, 'User account was successfully created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.success(request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
