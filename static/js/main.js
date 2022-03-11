7db5e0d6f5e85a31a5bafc9423b7f458291dff0c
Anto Ark <tuyenngo7bh@gmail.com>
2022-03-10 20:29:59 -0800

    add console log in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ if (searchForm){
     for (let i=0; pageLinks.length > i; i++){
         pageLinks[i].addEventListener('click', function (e) {
         e.preventDefault()
-
+        console.log('Button click')
         // Get the data attribute
         let page = this.dataset.page
 
@@ -16,7 +16,6 @@ if (searchForm){
 
         // Submit form
         searchForm.submit()
-
     })
     }
 }
