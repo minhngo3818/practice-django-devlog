2c856f786aa7e31e1766a3491ff6a037c5b08c2b
Anto Ark <tuyenngo7bh@gmail.com>
2022-03-27 18:31:00 -0700

    update reset password

--- practice_django/urls.py
+++ practice_django/urls.py
@@ -26,16 +26,17 @@ urlpatterns = [
     path('', include('users.urls')),
 
     # Set-up password recovery paths
-    path('reset_password/', auth_views.PasswordResetView.as_view(),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
          name="reset_password"),        # Must type correct name convention
+                                        # template_name="your custome template"
 
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
          name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"),
          name="password_reset_confirm"),
 
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),
          name="password_reset_complete"),
 ]
 
