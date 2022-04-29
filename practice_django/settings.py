ce0fff3cc5437a38185b6ac89c766f93245edec8
Anto Ark <tuyenngo7bh@gmail.com>
2022-04-28 19:24:19 -0700

    Final commit
    dummy message 2
    
    update delete by using javascript
    
    Update settings.py
    
    dummy message

--- practice_django/settings.py
+++ practice_django/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+# Add commentsdxvzXvxzcv
+# Add commentsdxvzXvxzcv
+# Add commentsdxvzXvxzcv
 
 from pathlib import Path
 from datetime import timedelta
